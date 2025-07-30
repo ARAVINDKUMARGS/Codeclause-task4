1aa22df232cb4587aa88428dee3b92fee6619719
ARAVINDKUMARGS <aravindkumar06062006@gmail.com>
2025-07-30 21:37:33 +0530

    Create Input and Output.py

---
+++ Input and Output.py
@@ -0,0 +1,7 @@
+I am a creative team leader who has experience in planning events, collaborating with peers, and mentoring juniors. I enjoy research and learning new skills.
+Predicted Personality Scores:
+Extraversion: 2
+Agreeableness: 2
+Conscientiousness: 1
+Openness: 3
+Emotional Stability: 0
